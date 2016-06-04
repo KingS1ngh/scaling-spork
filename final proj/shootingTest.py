4953b32b49684b2ae6914703e54e5184e8bb8c5e
KingS1ngh <hellohibyebye234@gmail.com>
2016-06-04 16:03:47 -0400

    gkfdkgjk

--- final proj/shootingTest.py
+++ final proj/shootingTest.py
@@ -4,6 +4,8 @@ from pygame import *
 from random import *
 from math import *
 #MA NAME JEF
+
+#gfkdjgkfjdkgjkfdjkf
 screen=display.set_mode((800,600))
 back1=image.load("back1.png").convert()
 back1=transform.scale(back1,(2000,2000))#lolololololololololololololollolololololol
